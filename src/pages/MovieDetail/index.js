732fbd95bab222fdc8d402107f41e39929486f62
Meera Jose Mampilly <meerajm@gmail.com>
2020-10-29 20:28:26 +0100

    change play icon

--- src/pages/MovieDetail/index.js
+++ src/pages/MovieDetail/index.js
@@ -10,6 +10,9 @@ const MovieDetails = () => {
           url="https://www.youtube.com/watch?v=03-KVRmd3xo"
           controls="true"
           className="react-player"
+          playing
+          playIcon={<button type="button">&#9658;</button>}
+          light="https://m.media-amazon.com/images/M/MV5BNThhYzE5MDUtODYyMC00ZWI3LTk0ZTgtNDgyMmYxM2UzZjBmXkEyXkFqcGdeQXVyNjE1OTQ0NjA@._V1_SX300.jpg"
         />
       </div>
     </>
